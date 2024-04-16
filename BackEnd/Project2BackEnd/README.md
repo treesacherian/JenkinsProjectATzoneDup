abec0df13957abecd276eab6d7ab88bed938becb
treesacherian <150167904+treesacherian@users.noreply.github.com>
2024-04-16 15:43:35 +0100

    Add files via upload

---
+++ BackEnd/Project2BackEnd/README.md
@@ -0,0 +1,2 @@
+# Back End code for project 2 during incubation.
+## Shop application for company CCZone.
---
+++ BackEnd/Project2BackEnd/src/main/java/com/lbg/cczone/domain/Item.java
@@ -0,0 +1,78 @@
+package com.lbg.cczone.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String itemName;
+	private Double itemPrice;
+	private Integer itemQuantity;
+	private String image;
+	@JsonBackReference
+	@ManyToOne
+	private Cart cart;
+
+	public Item() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Double getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(Double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public Integer getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(Integer itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+}
